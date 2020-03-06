16454f8f627afe600ca44b70f4dd677e0c5f491f
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-03-06 09:16:35 -0500

    Get code out of the header

--- melcate.h
+++ melcate.h
@@ -8,13 +8,6 @@ namespace melcate {
 		private T* allocloc;
 		private bool alloced;
 		private bool exis;
-		void initmem() {
-			if (! this->alloced) {
-				this->allocloc = malloc(sizeof(T));
-				if (this->allocloc == 0) {
-						std::cerr << "[fatal error @ " << __FILE__ << ":" << __LINE__ << "] out of memory (need to allocate " << std::to_string(sizeof(T)) << ")";
-						std::abort();
-				} else
-						this->alloced = true;
-			}
-		}
+		void initmem();
+    }
+}
