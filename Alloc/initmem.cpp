1541831f6f8ccd0838451cbaa89b166ff5c8d1d7
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-03-06 09:26:33 -0500

    Create void melcate::Alloc::initmem()

---
+++ Alloc/initmem.cpp
@@ -0,0 +1,14 @@
+namespace melcate {
+	void Alloc::initmem() {
+		if (! this->alloced) {
+			this->allocloc = malloc(sizeof(T));
+			if (this->allocloc == 0) {
+					std::cerr << "[fatal error @ " << __FILE__ << ":" << __LINE__ << "] out of memory (need to allocate " << std::to_string(sizeof(T)) << ")";
+					std::abort();
+			} else {
+				this->alloced = true;
+            }
+		}
+	}
+ 
+ }
